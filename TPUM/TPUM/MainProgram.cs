a626a657cc9d06bd4f32e8a601c29be1dcd68ad7
DevJacek112 <jacekteodorczyk01@gmail.com>
2025-03-26 19:46:52 +0100

    Added example program.

---
+++ TPUM/TPUM/MainProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPUM
+{
+    internal class MainProgram
+    {
+        public static void Main()
+        {
+            Calculator calculatorObject = new Calculator();
+            Console.Write(calculatorObject.Add(2, 3));
+        }
+    }
+}
---
+++ TPUM/TPUMTests/Test1.cs
@@ -0,0 +1,20 @@
+﻿namespace TPUMTests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethodAdd()
+        {
+            TPUM.Calculator calculator = new TPUM.Calculator();
+            Assert.AreEqual(calculator.Add(2, 3), 5);
+        }
+
+        [TestMethod]
+        public void TestMethodSub()
+        {
+            TPUM.Calculator calculator = new TPUM.Calculator();
+            Assert.AreEqual(calculator.Sub(6, 2), 4);
+        }
+    }
+}
