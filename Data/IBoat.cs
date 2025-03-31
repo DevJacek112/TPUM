bb344722bc8fc1c0d78cfa725af547b798d28257
Piotr Marszałek <thewojwymx@o2.pl>
2025-03-31 15:42:20 +0200

    Logic layer implemented

---
+++ Data/IBoat.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Data
+{
+    public abstract class IBoat : ISerializable
+    {
+        public abstract int Id { get; }
+        public abstract string Name { get; }
+        public abstract string Description { get; }
+        public abstract float Price { get; set; }
+
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("ID: ", Id);
+            info.AddValue("Name: ", Name);
+            info.AddValue("Description:", Description);
+            info.AddValue("Price: ", Price);
+        }
+
+
+    }
+}
