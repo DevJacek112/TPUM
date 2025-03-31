b11afa41ea7f5dc0be09385e4a782211f2c857ef
Piotr Marszałek <thewojwymx@o2.pl>
2025-03-31 16:25:24 +0200

    Logic Layer implemented in 50%

---
+++ Logic/AbstractLogicAPI.cs
@@ -0,0 +1,56 @@
+﻿using Data;
+
+namespace Logic
+{
+    public abstract class AbstractLogicAPI
+    {
+        public static AbstractLogicAPI createInstance(AbstractDataAPI? dataAPI = null)
+        {
+            return new LogicAPI(dataAPI ?? AbstractDataAPI.createInstance());
+        }
+
+        public abstract List<IBoat> GetAllBoats();
+        public abstract void addBoat(string name, string description, float price);
+        public abstract bool buyBoat(int id);
+
+
+        private class LogicAPI : AbstractLogicAPI
+        {
+            private readonly AbstractDataAPI myDataAPI;
+            private int IdCounter = 1;
+                
+            public LogicAPI(AbstractDataAPI? dataAPI)
+            {
+                myDataAPI = dataAPI;
+                IdCounter = dataAPI.GetAllBoats().Count + 1;
+            }
+
+            public override List<IBoat> GetAllBoats()
+            {
+                return myDataAPI.GetAllBoats();
+            }
+
+            public override void addBoat(string name, string description, float price)
+            {
+                myDataAPI.AddBoat(IdCounter++, name, description, price);
+            }
+
+            public override bool buyBoat(int id)
+            {
+                var boat = myDataAPI.GetBoatById(id);
+                if (boat == null)
+                {
+                    return false;
+                }
+
+                myDataAPI.RemoveBoat(id);
+                return true;
+            }
+
+        }
+       
+
+
+
+    }
+}
