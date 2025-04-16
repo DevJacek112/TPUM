32c027d50dea563de1ac865ecda01eb00390ec07
Piotr Marszałek <thewojwymx@o2.pl>
2025-04-16 23:04:49 +0200

    Reintroduced program entry

---
+++ ServerModel/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerModel
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var server = new ServerWebSocketAPI();
+            await server.StartAsync();
+        }
+    }
+}
